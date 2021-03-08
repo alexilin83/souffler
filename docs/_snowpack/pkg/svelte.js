82285122b10e519bdde3ad2ee4e7a9831232735b
Alex <alexilin83@gmail.com>
2021-03-08 19:35:44 +0300

    build

---
+++ docs/_snowpack/pkg/svelte.js
@@ -0,0 +1,1 @@
+export { o as onMount } from './common/index-8289820e.js';
---
+++ docs/_snowpack/pkg/svelte/internal.js
@@ -0,0 +1,1 @@
+export { S as SvelteComponent, a as append, b as attr, c as binding_callbacks, d as detach, e as element, i as init, f as insert, n as noop, s as safe_not_equal, g as space } from '../common/index-8289820e.js';
